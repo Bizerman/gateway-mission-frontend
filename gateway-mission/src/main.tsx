dc14ee193f30c8cdf698f66c307e36f08790a1c8
Bizarreman <lesha-utenkov@rambler.ru>
2025-02-08 23:02:25 +0300

    add

---
+++ gateway-mission/src/main.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ROUTES } from "../Routes";
+import HomePage from "./pages/HomePage.tsx";
+import Navigation from "./components/Navigation.tsx";
+import GatewayElementsPage from "./pages/GatewayElementsPage.tsx";
+import ElementPage from "./pages/ElementPage.tsx";
+
+function App() {
+  return (
+    <BrowserRouter basename="/gateway-mission-frontend">
+      <Navigation/>
+      <Routes>
+        <Route path={ROUTES.HOME} index element={<HomePage />} />
+        <Route path={ROUTES.GATEWAY_ELEMENTS} element={<GatewayElementsPage />} />
+        <Route path={ROUTES.GATEWAY_ELEMENT_DETAIL} element={<ElementPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
