8fe5cdbd55e065ed84d978f611766a4ec95473e9
Bizarreman <lesha-utenkov@rambler.ru>
2025-02-09 04:49:23 +0300

    Updates

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/gateway-mission-frontend/sw.js', { scope: '/gateway-mission-frontend/' })})}
